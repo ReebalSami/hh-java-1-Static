d60d751e0e82ad979ba761462690e4ba6b467bd2
emea94 <emea94@web.de>
2024-01-31 15:12:02 +0100

    create Multiple Objects and call both methods

--- src/main/java/neuefisched/de/Main.java
+++ src/main/java/neuefisched/de/Main.java
@@ -2,6 +2,13 @@ package neuefisched.de;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NewClass object = new NewClass();
+        NewClass object2 = new NewClass();
+        NewClass object3 = new NewClass();
+        NewClass object4 = new NewClass();
+
+        NewClass.incrementTotalCount();
+        object.incrementInstanceCount();
+
     }
 }
