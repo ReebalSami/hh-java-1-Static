54ee93ea910f121c8857b9ed000fc57e6b7b3aa4
mahambach <ma.hambach@gmx.net>
2024-01-31 15:03:04 +0100

    add static Method incrementTotalCount in Class NewClass

--- src/main/java/neuefisched/de/NewClass.java
+++ src/main/java/neuefisched/de/NewClass.java
@@ -2,5 +2,10 @@ package neuefisched.de;
 
 public class NewClass {
 
-    public static int totalCount = 0
+    public static int totalCount = 0;
+    public int instanceCount = 0;
+
+    public static void incrementTotalCount(){
+        totalCount++;
+    }
 }
